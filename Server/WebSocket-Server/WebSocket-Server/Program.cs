3676ddd520875b196e68a2a05e76ff871096a828
Kyle Pereira <54874045+kyleprr@users.noreply.github.com>
2023-07-17 00:24:43 +1000

    Add files via upload

---
+++ Server/WebSocket-Server/WebSocket-Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocket_Server;
+
+namespace WebSocketServer
+{
+    class WebSocketServer
+    {
+        static void Main(string[] args)
+        {
+            string serverURL = "http://localhost:1234/";
+            WebSocketHandler.StartListening(serverURL).Wait();
+        }
+    }
+}
---
+++ Server/WebSocket-Server/WebSocket-Server/WebSocketHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocket_Server
+{
+    internal class WebSocketHandler
+    {
+
+        // Asynchronous task that starts the WebSocket server //
+        public static async Task StartListening(string url)
+        {
+            var httpListener = new HttpListener();
+            httpListener.Prefixes.Add(url);
+            httpListener.Start();
+
+            Console.WriteLine("WebSocket server started...");
+
+        }
+
+
+
+
+    }
+}
